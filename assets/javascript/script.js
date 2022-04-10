d16df7c7e83a1fb6608797b49e58b942270e4604
Sérgio Marques <sergiogamerbrx@gmail.com>
2022-04-10 20:51:41 -0300

    botão apagar lembrete

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,7 @@
 const inputTask = window.document.querySelector('input');
 const buttonTask = window.document.querySelector('.button');
 const taskArea = window.document.querySelector('.task-area');
+const deleteTask = window.document.querySelectorAll('.delete-task');
 
 const addElement = () => {
   if (inputTask.value != '') {
@@ -9,6 +10,15 @@ const addElement = () => {
     let task = taskArea.appendChild(createTask);
     task.innerText = inputTask.value;
     task.setAttribute('class', 'task');
+    inputTask.value = '';
+    inputTask.focus();
+    // delete-task
+    let delTask = window.document.createElement('div');
+    let svgTask = window.document.createElement('ion-icon');
+    svgTask.setAttribute('name', 'trash-outline');
+    delTask.setAttribute('class', 'delete-task');
+    delTask.appendChild(svgTask);
+    createTask.appendChild(delTask);
     // decoration.
     document.querySelector('span').style.display = 'none';
     taskArea.style.justifyContent = 'flex-start';
@@ -16,3 +26,16 @@ const addElement = () => {
 };
 
 buttonTask.addEventListener('click', addElement);
+
+console.log(deleteTask);
+
+window.document.querySelectorAll('.delete-task').forEach(item => {
+  item.addEventListener('click', deleteTaskFunction);
+  console.log('dwad');
+});
+
+function deleteTaskFunction() {
+  // this.style.backgroundColor = 'purple';
+  console.log('teste');
+};
+
